3be336e6924519c39014045723a71f63dd406fa6
Gonderage <78834252+Gonderage@users.noreply.github.com>
2023-04-19 01:03:23 -0400

    Add project files

---
+++ Pet.gd
@@ -0,0 +1,11 @@
+extends Node2D
+class_name Pet
+
+var pet_name:String = "Pet"
+var pet_description:String = "Description"
+var health:int = 1
+var attack:int = 1
+var tier:int = 1
+var experience:int = 0
+var buy_price:int = 3
+var sell_price:int = 1
---
+++ shop_manager.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+@export var pet_template:Node
+@export var food_template:Node
+@export var start_coins:int = 10
+
+var current_coins = 10
+var current_tier:int = 1
+var shop_upgrade:int = 0
+var pet_discount:int = 0
+var food_discount:int = 0
+var max_buy_price:int = 3
+
+func spawn_pet(pet_name:String, parent):
+	var path = "res://pets/" + pet_name + ".json"
+	var file = FileAccess.open(path, FileAccess.READ)
+	var _json = JSON.parse_string(file.get_as_text())
+	
+	#BRING FORTH YOUR STRONGEST PET from the json data
+	var new_pet = pet_template.duplicate()
+	new_pet.set_scricpt(load(_json["script"]))	#Script goes first or you can not set the variables.
+	new_pet.pet_name = _json["name"]
+	new_pet.pet_description = _json["description"]
+	new_pet.attack = clampi(_json["attack"] + shop_upgrade, 1, 50)
+	new_pet.health = clampi(_json["health"] + shop_upgrade, 1, 50)
+	new_pet.tier = _json["tier"]
+	new_pet.buy_price = clampi(max_buy_price - pet_discount, 0, max_buy_price)
+	new_pet.texture = load(_json["sprite"])
+	new_pet.position = get_global_mouse_position()
+	
+	#Add to the scene
+#	parent.add_child(new_pet)
+
+func spawn_food(food_name:String):
+	var path = "res://food/" + food_name + ".json"
+	var file = FileAccess.open(path, FileAccess.READ)
+	var _json = JSON.parse_string(file.get_as_text())
+	
+	var new_food = food_template.duplicate()
+	new_food.set_script(load(_json["script"]))
+	new_food.food_name = _json["name"]
+	new_food.food_description = _json["description"]
+	new_food.buy_price = clampi(max_buy_price - food_discount, 0, max_buy_price)
+	new_food.texture = load(_json["sprite"])
